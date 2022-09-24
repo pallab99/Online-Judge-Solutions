66cb0a46813f7f0fb4090c53ea89b08abb865c35
Pallab Majumdar <37453678+pallab99@users.noreply.github.com>
2022-09-25 01:50:58 +0600

    Delete Codeforces directory

--- Codeforces/208/A.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    string s;
-    cin>>s;
-    regex l("WUB");
-    cout<<regex_replace(s,l," ");
-}
--- Codeforces/50/A.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int M,N,n;
-    scanf("%d %d",&M,&N);
-    n=M*N;
-    int ans=n/2;
-    printf("%d",ans);
-}
